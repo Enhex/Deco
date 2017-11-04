5a52017f0ad83ad5e83f261e961cda318d1ee5b0
Enhex <enhex0@gmail.com>
2017-11-04 04:11:55 +0200

    CNVP: use string_view, and fix redundant temporary string.

--- include/deco/NVP.h
+++ include/deco/NVP.h
@@ -29,14 +29,14 @@ namespace deco
 	// Content name value pair: NVP inside the entry content, using the structure delimiter to separate between name and value, which means it can't be inside the name
 	template<typename T>
 	struct CNVP {
-		std::string name;
+		std::string_view name;
 		T& value;
 	};
 
 	template<typename T>
-	auto make_CNVP(std::string&& name, T& value) {
+	auto make_CNVP(std::string_view&& name, T& value) {
 		return CNVP<T>{
-			std::forward<std::string>(name),
+			std::forward<std::string_view>(name),
 				value	// want to always use T member, so no forwarding
 		};
 	}
@@ -61,28 +61,26 @@ namespace gs
 		serialize(stream, deco::skip);	// skip set entry name
 		serialize(stream, nvp.value);	// read child entry
 		if(stream.current_entry.type != deco::Entry::set_end)
-			stream.parse_entry();		// skip set end if child didn't
-		//TODO vector<T> already reads the set end, other types may not
+			stream.parse_entry();		// skip set end if T didn't
 	}
 
 
 	// CNVP
-	template<typename Stream>
-	typename std::enable_if_t<std::is_base_of_v<deco::OutputStream, Stream>>
-	write(Stream& stream, const deco::CNVP<std::string>& nvp)
-	{
-		serialize(stream, nvp.name + ": " + deco::escape_content(nvp.value));
-	}
-
 	template<typename Stream, typename T>
 	typename std::enable_if_t<std::is_base_of_v<deco::OutputStream, Stream>>
 	write(Stream& stream, const deco::CNVP<T>& nvp)
 	{
-		if constexpr(std::is_floating_point_v<T>)
-			serialize(stream, (nvp.name + ": ") += deco::to_string(nvp.value));
-			//serialize(stream, (nvp.name + ": ") += deco::trim_float(std::to_string(nvp.value)));
+		const auto str = [&nvp]() {
+			return std::string(nvp.name) += ": ";
+		};
+
+		if constexpr (std::is_same_v<T, std::string>)
+			serialize(stream, str() += deco::escape_content(nvp.value));
+		else if constexpr (std::is_floating_point_v<T>)
+			serialize(stream, str() += deco::to_string(nvp.value));
+			//serialize(stream, str() += deco::trim_float(std::to_string(nvp.value)));
 		else
-			serialize(stream, (nvp.name + ": ") += std::to_string(nvp.value));
+			serialize(stream, str() += std::to_string(nvp.value));
 	}
 
 
