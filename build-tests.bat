a420e669abab6e56a77eb0f964aae38169881619
Enhex <enhex0@gmail.com>
2017-11-18 21:10:02 +0200

    refactor tests building

--- build-tests.bat
+++ build-tests.bat
@@ -1,7 +1,6 @@
 cd ..
-mkdir build
-cd build
+mkdir tests-build
+cd tests-build
 
 conan install ../Deco/tests
-conan install ../Deco/tests -s build_type=Debug
 cmake ../Deco/tests -G "Visual Studio 15 Win64"
