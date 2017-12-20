0a24f9a9b88651b21169c4a02effbb01e9680f43
Enhex <enhex0@gmail.com>
2017-12-20 14:56:46 +0200

    removed the need to use read_content

--- include/deco/NVP.h
+++ include/deco/NVP.h
@@ -12,17 +12,21 @@ namespace deco
 		std::enable_if_t<std::is_base_of_v<OutputStream, std::decay_t<Stream>>>
 		write_NVP(Stream& stream, const NVP<T>& nvp)
 	{
-		const auto str = [&nvp]() {
+		const auto name_str = [&nvp]() {
 			return std::string(nvp.name) += ": ";
 		};
 
+		/*NOTE:
+		The name's start and value's end are automatically escaped by the std::string serialization.
+		The value's internal sub-string start isn't escaped when serialized, so it needs to be escaped manually.
+		The name's end doesn't need escaping.
+		*/
 		if constexpr (std::is_same_v<std::decay_t<T>, std::string>)
-			serialize(stream, str() += escape_content(nvp.value));
-		else if constexpr (std::is_floating_point_v<T>)
-			serialize(stream, str() += to_string(nvp.value));
-			//serialize(stream, str() += trim_float(std::to_string(nvp.value)));
+			serialize(stream, name_str() += escape_content<true, false>(nvp.value));
+		else if constexpr (std::is_floating_point_v<T> || std::is_integral_v<T>)	// no need to escape
+			serialize(stream, name_str() += to_string(nvp.value));
 		else
-			serialize(stream, str() += std::to_string(nvp.value));
+			serialize(stream, name_str() += escape_content<true, false>(to_string(nvp.value)));
 	}
 
 
@@ -52,24 +56,27 @@ namespace deco
 
 	template<typename T, typename I> constexpr
 	void read(InputStream<I>& stream, NVP_t<T>& nvp)
-	{	
-		auto content = stream.parse_entry().content;
-
+	{
 		// skip whitespace
-		const auto whitespace_end = skip_whitespace(content.begin());
-		content.remove_prefix(std::distance(content.begin(), whitespace_end));
-
+		skip_whitespace(stream.position);
+		
+		// skip content begin delimiter
+		if (*stream.position == content_delimiter)
+			++stream.position;
+				
 		// read name
-		const auto name_end = content.begin() + content.find(':');
-		const auto name_length = std::distance(content.begin(), name_end);
-		nvp.name = content.substr(0, name_length);
-		content.remove_prefix(name_length + 1);	// +1 for ':'
+		const auto name_begin = stream.position;
 
-		// unescape delimiters chars
-		unescape_content<true>(content);
+		while (*stream.position != ':')
+			++stream.position;
 
-		// parse value
-		serialize(content, nvp.value);
+		nvp.name = std::string(name_begin, stream.position);
+
+		// skip NVP delimiter
+		++stream.position;	// +1 for ':'
+
+		// read value
+		serialize(stream, nvp.value);
 	}
 	template<typename T, typename I> constexpr
 		void read(InputStream<I>& stream, NVP_t<T>&& nvp)
@@ -104,17 +111,11 @@ namespace deco
 	template<typename T, typename I> constexpr
 		void read(InputStream<I>& stream, NVP_ignore_name_t<T>& nvp)
 	{
-		auto content = stream.parse_entry().content;
+		// skip name
+		while (*(stream.position++) != ':');
 
-		// skip whitespace
-		const auto whitespace_end = skip_whitespace(content.begin() + content.find(':') + 1);
-		content.remove_prefix(std::distance(content.begin(), whitespace_end));
-
-		// unescape delimiters chars
-		unescape_content<true>(content);
-
-		// parse value
-		serialize(content, nvp.value);
+		// read value
+		serialize(stream, nvp.value);
 	}
 	template<typename T, typename I> constexpr
 		void read(InputStream<I>& stream, NVP_ignore_name_t<T>&& nvp)
