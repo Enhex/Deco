fba757cf85052b243c6dbf4b6272b24554f98c95
Enhex <enhex0@gmail.com>
2019-02-11 00:25:27 +0200

    switch to bincrafters' Boost Spirit

--- conanfile.py
+++ conanfile.py
@@ -10,10 +10,9 @@ class DecoConan(ConanFile):
 	requires = (
 		"generic_serialization/master@enhex/stable",
 		"rang/master@enhex/stable",
-		"Boost/1.66.0@enhex/testing",
+		"boost_spirit/1.66.0@bincrafters/stable",
 		"strong_type/master@enhex/stable"
 	)
-	default_options = "Boost:header_only=True"
 	# No settings/options are necessary, this is header only
 	exports_sources = "include/*"
 
