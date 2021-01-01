9ff974d6b45257604fd18bae0ea4ed95272da58d
Enhex <enhex0@gmail.com>
2021-01-01 10:01:05 +0200

    remove ill formed test case.
    the child vectors aren't placed inside a deco list.

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -10,14 +10,12 @@
 int main()
 {
 	const std::vector<std::string> val{ "1", "2", "3", "4", "5" };
-	const std::vector<std::vector<std::string>> val2{ {"1", "2"}, {"3", "4", "5"} };
 
 	// write
 	{
 		deco::OutputStream_indent stream;
-		
+
 		deco::serialize(stream, deco::make_list("std::vector", val));
-		deco::serialize(stream, deco::make_list("std::vector2", val2));
 
 		std::ofstream os("out.deco", std::ios::binary);
 		os << stream.str;
@@ -36,7 +34,5 @@ int main()
 
 		std::vector<std::string> in_val;
 		deco::serialize(stream, deco::make_list("std::vector", in_val)); assert(in_val == val);
-		std::vector<std::vector<std::string>> in_val2;
-		deco::serialize(stream, deco::make_list("std::vector2", in_val2)); assert(in_val2 == val2);
 	}
 }
