e730ad7e61d74be56237dc2a5eb7cf807a0fa305
Enhex <enhex0@gmail.com>
2021-09-15 02:24:30 +0300

    upgrade dependencies

--- conanfile.py
+++ conanfile.py
@@ -9,8 +9,8 @@ class DecoConan(ConanFile):
 	description = "Delimiter Collision Free Format"
 	requires = (
 		"generic_serialization/master@enhex/stable",
-		"rang/3.1.0",
-		"boost/1.76.0",
+		"rang/3.2",
+		"boost/1.77.0",
 		"strong_type/master@enhex/stable"
 	)
 	# No settings/options are necessary, this is header only
