71c844fa05815f1ac1f5471a6f48f640cab01756
Enhex <enhex0@gmail.com>
2022-01-04 16:55:13 +0200

    upgrade boost to 1.78.0

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class DecoConan(ConanFile):
 	requires = (
 		"generic_serialization/master@enhex/stable",
 		"rang/3.2",
-		"boost/1.77.0",
+		"boost/1.78.0",
 		"strong_type/master@enhex/stable"
 	)
 	# No settings/options are necessary, this is header only
